15a1a2cfe5e1031b8f15501595fcaea2c1c77122
Rafael Aguiar <terule@gmail.com>
2023-01-08 07:15:55 -0300

    Alterado CSS da HOME e HEADER: Adicionado novo estilo nos botões, adicionada font Roboto ao projeto

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,25 +5,26 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="home">
-      <p>
-        Essa página é dedicada para cadastro de clínicas de saúde mental
-        para ajuda de acordo com a sua localidade. Dedicada à alunos da Trybe.
-      </p>
-      <p>
-        Aqui, queremos construir um banco de dados de locais com
-        assistência psicológica gratuita ou de custo popular, então sinta-se livre para contribuir.
-      </p>
-      <div>
-        <Link to="/list">Clique aqui para ver a lista de locais</Link>
+    <>
+      <div className="container">
+        <div className="home">
+          <p>
+            Essa página é dedicada para cadastro de clínicas de saúde mental
+            para ajuda de acordo com a sua localidade. Dedicada à alunos da Trybe.
+          </p>
+          <p>
+            Aqui, queremos construir um banco de dados de locais com
+            assistência psicológica gratuita ou de custo popular, então sinta-se
+            livre para contribuir.
+          </p>
+          <div className="links">
+            <Link to="/list">Clique aqui para ver a lista de locais</Link>
+            <Link to="/register">Clique aqui para contribuir e adicionar um local ao nosso banco</Link>
+          </div>
+        </div>
       </div>
-      <div>
-        <Link to="/register">Clique aqui para contribuir e adicionar um local ao nosso banco</Link>
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 

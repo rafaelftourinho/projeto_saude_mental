7e39ef650e82b78fc02582e3e34a86b12ceac161
rafaelftourinho <rafaelftourinho@gmail.com>
2023-01-09 22:26:10 -0300

    :bug: fixing bug

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,9 +8,9 @@ function Footer() {
       <a href="https://github.com/rafaelftourinho" target="blank">Rafael França</a>
       ,
       <a href="https://github.com/Terule" target="blank">Rafael Aguiar</a>
+      ,
       <a href="https://github.com/daniellegazarini" target="blank">Danielle Gazarini</a>
-      {/* COLOQUEM OS SEUS DADOS AQUI TAMBÉM! */}
-      {/* PRECISA ESTILIZAR ISSO PRA ONTEM, ASSIM COMO TODA A PALETA DE CORES! HAHAHAHA */}
+
     </div>
   );
 }

a70853f882e3eef068eabede3388c4fc8254c322
Danielle Gazarini <daniellegazarini@gmail.com>
2023-01-10 12:12:05 -0300

    add register css

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const conn = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   database: process.env.MYSQL_DATABASE || 'cadastro',
-  port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT || '3306',
 });
 
 module.exports = conn;

b1d44cdbd0bd1d4e0a6a7d8c94df6b2ad56fc2fe
Danielle Gazarini <daniellegazarini@gmail.com>
2023-01-16 13:39:03 -0300

    css cadastro

---
+++ backend/prisma/seed.js
@@ -0,0 +1,11 @@
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   const locais = await prisma.local.create({
+//     data: {
+//       nome: ""
+//     }
+//   });
+
+// }
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -13,22 +13,34 @@ function Register() {
   const createNewInfo = async (event) => {
     event.preventDefault();
     const {
+      nome,
+      preco,
+      cep,
+      rua,
+      numero,
+      complemento,
+      bairro,
       cidade,
       estado,
-      endereco,
+      email,
       telefone,
-      contatos,
-      preco,
+      whatsapp,
     } = infos;
 
     try {
       await InfoFetch.post('/infos', {
+        nome,
+        preco,
+        cep,
+        rua,
+        numero,
+        complemento,
+        bairro,
         cidade,
         estado,
-        endereco,
+        email,
         telefone,
-        contatos,
-        preco,
+        whatsapp,
       });
     } catch (error) {
       console.log(error);
@@ -46,24 +58,157 @@ function Register() {
 
   return (
     <div className="new-info">
-      <h4>Inserir novas informações</h4>
       <form onSubmit={(e) => createNewInfo(e)}>
-        <div className="form-control">
-          {arr.map((info) => (
-            <div key={info}>
-              <label htmlFor={info}>
-                {info}
+        <fieldset>
+          <legend>Novo Cadastro</legend>
+          <div>
+            <div className="form-group" id="form-1">
+              <label htmlFor="input_name">
+                Nome do(a) profissional ou Clínica
+                <input
+                  type="text"
+                  className="form-control"
+                  id="input_name"
+                  placeholder="Nome do(a) profissional ou Clínica"
+                />
               </label>
+            </div>
+          </div>
+          <div className="form-group" id="form-2">
+            <div className="row">
+              <label htmlFor="input_price">
+                Valor:
+                <input
+                  type="text"
+                  className="form-control"
+                  id="input_price"
+                  placeholder="100,00"
+                />
+              </label>
+            </div>
+            <label id="label_cep" htmlFor="input_cep">
+              CEP:
               <input
                 type="text"
-                name={info.toLowerCase()}
-                id={info}
-                placeholder={`Digite o ${info}`}
-                onChange={handleChange}
+                className="form-control"
+                id="input_cep"
+                placeholder="05012-000"
               />
+            </label>
+          </div>
+          <div className="form-group" id="form-3">
+            <div className="row">
+              <label htmlFor="input_street">
+                Rua:
+                <input
+                  type="text"
+                  className="form-control"
+                  id="input_street"
+                  placeholder="Rua Cardoso de Almeida"
+                />
+              </label>
             </div>
-          ))}
-        </div>
+            <label id="label_cep" htmlFor="input_cep">
+              Número:
+              <input
+                type="text"
+                className="form-control"
+                id="input_cep"
+                placeholder="547"
+              />
+            </label>
+          </div>
+          <div className="form-group" id="form-4">
+            <div className="row">
+              <label htmlFor="input_street">
+                Complemento:
+                <input
+                  type="text"
+                  className="form-control"
+                  id="input_street"
+                  placeholder="Rua Cardoso de Almeida"
+                />
+              </label>
+            </div>
+            <label id="label_cep" htmlFor="input_cep">
+              Bairro:
+              <input
+                type="text"
+                className="form-control"
+                id="input_cep"
+                placeholder="547"
+              />
+            </label>
+          </div>
+          <div className="form-group" id="form-5">
+            <div className="row">
+              <label htmlFor="input_street">
+                Cidade:
+                <input
+                  type="text"
+                  className="form-control"
+                  id="input_street"
+                  placeholder="Rua Cardoso de Almeida"
+                />
+              </label>
+            </div>
+            <label id="label_estado" htmlFor="input_cep">
+              Estado:
+              <select>
+                <option value="estado">Estado</option>
+              </select>
+            </label>
+          </div>
+          <div className="form-group" id="form-6">
+            <label htmlFor="input_name">
+              Email:
+              <input
+                type="text"
+                className="form-control"
+                id="input_name"
+                placeholder="email@email.com"
+              />
+            </label>
+          </div>
+          <div className="form-group" id="form-7">
+            <div className="row">
+              <label htmlFor="input_tel">
+                Telefone:
+                <input
+                  type="text"
+                  className="form-control"
+                  id="input_tel"
+                  placeholder="(11) 91234-5678"
+                />
+              </label>
+            </div>
+            <label id="label_whats" htmlFor="input_whats">
+              WhatsApp:
+              <input
+                type="text"
+                className="form-control"
+                id="input_whats"
+                placeholder="(11) 98541-5623"
+              />
+            </label>
+          </div>
+        </fieldset>
+
+        {/* {arr.map((info) => (
+          <div key={info}>
+            <label htmlFor={info}>
+              {info}
+            </label>
+            <input
+              type="text"
+              name={info.toLowerCase()}
+              id={info}
+              placeholder={`Digite o ${info}`}
+              onChange={handleChange}
+            />
+          </div>
+        ))} */}
+
         <input type="submit" value="Enviar informações" className="btn" />
       </form>
     </div>
